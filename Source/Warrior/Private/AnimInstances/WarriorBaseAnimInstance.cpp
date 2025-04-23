c375907f769aa605d4315e49fc75943abd965808
RJD27 <76191985+RJD27@users.noreply.github.com>
2025-04-22 22:24:39 -0500

    Implemented Anim Instance on MC
    Implemented the idle and relaxed animinstance to the MC

---
+++ Source/Warrior/Private/AnimInstances/WarriorBaseAnimInstance.cpp
@@ -0,0 +1,5 @@
+// Randy De Leon all rights reserved.
+
+
+#include "AnimInstances/WarriorBaseAnimInstance.h"
+
---
+++ Source/Warrior/Private/AnimInstances/WarriorCharacterAnimInstance.cpp
@@ -0,0 +1,27 @@
+// Randy De Leon all rights reserved.
+
+
+#include "AnimInstances/WarriorCharacterAnimInstance.h"
+#include "Characters/WarriorBaseCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UWarriorCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	OwningCharacter = Cast<AWarriorBaseCharacter>(TryGetPawnOwner());
+
+	if (OwningCharacter)
+	{
+		OwningMovementComponent = OwningCharacter->GetCharacterMovement();
+	}
+}
+
+void UWarriorCharacterAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	if (!OwningCharacter || !OwningMovementComponent){ return; }
+
+	GroundSpeed = OwningCharacter->GetVelocity().Size2D();
+
+	bHasAcceleration = OwningMovementComponent->GetCurrentAcceleration().SizeSquared2D()>0.f;
+}
